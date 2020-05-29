f8b4fe23e50933d4266526b47a806698b963c771
Vern DeMille <vern.demille@crownpeak.com>
2020-05-28 18:54:00 -0600

    Changing default and providing caps to fix cidr block waste.

--- main.tf
+++ main.tf
@@ -6,9 +6,10 @@ terraform {
 }
 
 # Here for testing
-# provider "aws" {
-#   region = "us-west-1"
-# }
+provider "aws" {
+  region = "us-west-1"
+  # region = "us-east-1"
+}
 
 data "aws_availability_zones" "azs" {
   state = "available"
@@ -19,7 +20,7 @@ module "shared" {
 }
 
 locals {
-  az_count = length(data.aws_availability_zones.azs.names)
+  az_count = min(length(data.aws_availability_zones.azs.names), var.max_azs_to_use)
 
   # To force terraform to loop arbitrarily, this a custom list of numbers that work as indexes.
   groups = module.shared.loopable[var.num_subnet_groups]
@@ -28,7 +29,7 @@ locals {
     for group in local.groups :
     [
       #This creates the next number of subnets in the cidr. It uses the index function to assing an increment number based on the az name. It then adds the az buffer and for each each additional group, the group offset.
-      for name in data.aws_availability_zones.azs.names : cidrsubnet(var.base_cidr, var.cidr_bit_mask, index(data.aws_availability_zones.azs.names, name) + ((var.az_addition_buffer + local.az_count) * local.groups[group]))
+      for name in data.aws_availability_zones.azs.names : cidrsubnet(var.base_cidr, var.cidr_bit_mask, index(data.aws_availability_zones.azs.names, name) + (min(var.max_azs_to_use, (var.az_addition_buffer + local.az_count)) * local.groups[group]))
     ]
   ]
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,18 @@
 variable "az_addition_buffer" {
   type        = number
-  description = "The numbers of \"placeholders\" to leave between subnets. This will allow you to adjust if a new az happens."
-  default     = 4
+  description = "The numbers of \"placeholders\" to leave between subnets. This will allow you to adjust if a new az happens for a newer or smaller region."
+  default     = 3
+}
+
+variable "max_azs_to_use" {
+  type        = number
+  description = "The maximum number of azs to allow, az_addition_buffer will not pass this number. Defaults to match us-east-1 (at time of writing), but provide padding for others. Set to a high number to disable cap. Here to hard cap cidr block usage."
+  default     = 6
 }
 
 variable "base_cidr" {
   type        = string
-  description = "The base cidr that subnets are being calculated"
+  description = "The base cidr from which subnets are being calculated"
   default     = "10.10.0.0/16"
 }
 
@@ -19,5 +25,5 @@ variable "cidr_bit_mask" {
 variable "num_subnet_groups" {
   type        = number
   description = "The number of individual groups of subnets you want. EG for public and private, you'd want to use the value 2, where as if you need rds also, use the value 3."
-  default     = 4
+  default     = 3
 }
